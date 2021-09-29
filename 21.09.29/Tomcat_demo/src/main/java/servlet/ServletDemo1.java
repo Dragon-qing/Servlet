edf8f5664664c0fb278aecd1c0afde2db9a65a3f
DQ <1259337898@qq.com>
2021-09-29 23:13:25 +0800

    2021.09.29

---
+++ 21.09.29/Tomcat_demo/src/main/java/servlet/ServletDemo1.java
@@ -0,0 +1,37 @@
+package servlet;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+/**
+ * @author Dragon_qing
+ */
+public class ServletDemo1 implements Servlet {
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return null;
+    }
+    /**
+     * 提供服务的方法
+     */
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("hello Servlet");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
